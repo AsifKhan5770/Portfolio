771c9f6355528c43080bbdb69206ad250e95d395
AsifKhan5770 <subkaking123@gmail.com>
2025-08-02 23:31:57 +0530

    some changes

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,27 +1,20 @@
-
-$(document).ready(function(){
-$('.image-popup-vertical-fit').magnificPopup({
-  type: 'image',
-  mainClass: 'mfp-with-zoom', 
-  gallery:{
-      enabled:true
+$(document).ready(function() {
+  $('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    mainClass: 'mfp-with-zoom',
+    gallery: {
+      enabled: true
     },
-  image: {
+    image: {
       verticalFit: false
     },
-
-  zoom: {
-    enabled: true, 
-
-    duration: 300, // duration of the effect, in milliseconds
-    easing: 'ease-in-out', // CSS transition easing function
-
-    opener: function(openerElement) {
-
-      return openerElement.is('img') ? openerElement : openerElement.find('img');
-  }
-}
-
-});
-
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function(openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
 });
